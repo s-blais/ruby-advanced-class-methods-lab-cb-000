9d9ab3533064d7fbf1554bb3c6bdc692e7f78fc6
Stephen Blais <stephen@s-blais.com>
2019-08-28 22:31:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,8 +48,8 @@ class Song
 
   def self.new_from_filename (filename)
     song = Song.new
-    filename.chomp(".mp3")
-    filename_array = filename.split("-")
+    
+    filename_array = filename.chomp(".mp3").split("-")
     song.name = filename_array[1]
     song.artist_name = filename_array[0]
     song
