a9654db753b7bf4419a0ffe3e757040d73b2b4bd
Stephen Blais <stephen@s-blais.com>
2019-08-28 21:42:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     self.class.all << self
   end
 
-  def self.create (name, artist_name)
+  def Song.create (name, artist_name)
     song = Song.new
     song.name = name
     song.artist_name = artist_name
