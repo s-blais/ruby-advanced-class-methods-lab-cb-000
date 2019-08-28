fcb2bb465232fa12eb327a2f5f0753c76c1882dd
Stephen Blais <stephen@s-blais.com>
2019-08-28 22:08:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,12 +32,11 @@ class Song
   end
 
   def self.find_by_name (name)
-    self.all.find {|song| song.name == name}
+    return self.all.find {|song| song.name == name}
   end
 
   def self.find_or_create_by_name (name)
     if self.find_by_name (name)
-      return self
     else self.create_by_name (name)
     end
   end
